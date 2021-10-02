f30e05e6dbe79213aa5e8c1d292fc23094015ab2
betterfly <betterfly4202@gmail.com>
2021-10-03 03:40:52 +0900

    initial commit

---
+++ prodo/src/main/java/com/paystock/messaging/api/ProdoApplication.java
@@ -0,0 +1,13 @@
+package com.paystock.messaging.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdoApplication.class, args);
+	}
+
+}
